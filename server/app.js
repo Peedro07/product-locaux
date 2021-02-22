9ee6f0165a1d28f627f939b987b0e741343ca9d8
Julien PICQUET <dext.1234@gmail.com>
2021-02-22 15:53:12 +0100

    server ok user

---
+++ server/app.js
@@ -0,0 +1,74 @@
+const
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    session = require('express-session'),
+    cors = require('cors'),
+    errorhandler = require('errorhandler'),
+    mongoose = require('mongoose');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const app = express();
+
+app.use(cors());
+
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(require('method-override')());
+app.use(express.static(__dirname + '/public'));
+
+app.use(session({secret: 'conduit', cookie: {maxAge: 60000}, resave: false, saveUninitialized: false}));
+
+if (!isProduction) {
+  app.use(errorhandler());
+}
+
+if (isProduction) {
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  mongoose.connect('mongodb://localhost/product-locaux');
+  mongoose.set('debug', true);
+  console.log('Connexion à la bdd réussie')
+}
+
+require('./models/User');
+require('./config/passport');
+
+app.use(require('./routes'));
+
+app.use(function (req, res, next) {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+if (!isProduction) {
+  app.use(function (err, req, res, next) {
+    console.log(err.stack);
+
+    res.status(err.status || 500);
+
+    res.json({
+      'errors': {
+        message: err.message,
+        error: err
+      }
+    });
+  });
+}
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    'errors': {
+      message: err.message,
+      error: {}
+    }
+  });
+});
+
+const server = app.listen(process.env.PORT || 5000, function () {
+  console.log('Serveur lancé sur le port ' + server.address().port);
+});
--- server/route/user.route.js
+++
@@ -1,7 +0,0 @@
-module.exports = app => {
-    const user = require('../controller/User.controller');
-    const router = require('express').Router();
-
-    router.post('/user', user.create);
-    app.use('/api', router)
-}
